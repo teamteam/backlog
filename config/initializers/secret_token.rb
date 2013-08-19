ac045e771830b1a6b6732a315b35345abaecdca3
Michael Avila <me@michaelavila.com>
2013-08-19 10:29:29 -0700

    Add initial rails files
    These files were generated with the following command:
    
      $ rails new . --skip-test-unit
    
    --skip-test-unit was passed in because we are going to try out
    minitest-rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Backlog::Application.config.secret_key_base = '1b1e9a07d49abd2ab7c7a9fd86a9991c3d6f3b46e06037106500b4bf2ebcfb9c64453b43a692ab70978bcb48dc96318a7ec154d2222b688e4e738ff1d8e4029b'
