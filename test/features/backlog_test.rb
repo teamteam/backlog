34e9ec4f8e10f0e3caa35c5fa6124bcac3e63f47
Michael Avila <me@michaelavila.com>
2013-08-20 14:42:46 -0700

    Add scenario for viewing backlog item from backlog

--- test/features/backlog_test.rb
+++ test/features/backlog_test.rb
@@ -6,4 +6,13 @@ feature "Backlog" do
 
     assert page.status_code == 200
   end
+
+  scenario "can view a backlog item from the backlog" do
+    backlog_item = BacklogItem.create :name => "Backlog Item Name"
+    backlog_item = BacklogItem.create :name => "Backlog Item Name 2"
+
+    visit backlog_path
+    click_link backlog_item.name
+    page.has_content?(backlog_item.name)
+  end
 end
