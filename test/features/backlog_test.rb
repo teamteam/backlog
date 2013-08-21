530507337fca36b55d5d468587f8c6437bd831c0
Michael Avila <me@michaelavila.com>
2013-08-20 23:18:35 -0700

    Allow backlog items to be deleted from the backlog page

--- test/features/backlog_test.rb
+++ test/features/backlog_test.rb
@@ -6,4 +6,14 @@ feature "Backlog" do
 
     assert page.status_code == 200
   end
+
+  scenario "remove item from the backlog" do
+    backlog_item = backlog_items :first_item
+    backlog_items(:second_item).delete
+
+    visit backlog_path
+    page.click_on "Delete"
+
+    assert_nil BacklogItem.find_by_id backlog_item.id
+  end
 end
