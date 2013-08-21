9d002a435ee4f37b9757a84802eeba14339e140f
Michael Avila <me@michaelavila.com>
2013-08-21 14:20:10 -0700

    Add navigation
    Right now the navigation only moves you from the backlog item to the
    backlog although it stays visible on the backlog page as well.

--- test/views/backlog_items/edit.html.haml_test.rb
+++ test/views/backlog_items/edit.html.haml_test.rb
@@ -2,12 +2,17 @@ require "test_helper"
 
 describe BacklogItemsController do
   describe "edit" do
+    before :each do
+      @backlog_item = backlog_items :first_item
+      get :edit, :backlog_item_id => @backlog_item.id
+    end
+
     it "shows the name of the backlog item" do
-      backlog_item = backlog_items :first_item
+      @response.body.must_match /#{@backlog_item.name}/
+    end
 
-      get :edit, :backlog_item_id => backlog_item.id
-
-      @response.body.must_match /#{backlog_item.name}/
+    it "shows a link back to the backlog" do
+      assert_select "a[href=#{backlog_path}]", "Backlog"
     end
   end
 end
