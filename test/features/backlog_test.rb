c82072a94862a2dd78c03caa9d49eec1297494d2
Michael Avila <me@michaelavila.com>
2013-08-21 12:30:36 -0700

    Allow backlog items to be completed and reopened from backlog page

--- test/features/backlog_test.rb
+++ test/features/backlog_test.rb
@@ -16,4 +16,24 @@ feature "Backlog" do
 
     assert_nil BacklogItem.find_by_id backlog_item.id
   end
+
+  scenario "can complete backlog item from the backlog" do
+    backlog_item = backlog_items :first_item
+    backlog_items(:second_item).delete
+
+    visit backlog_path
+    click_on "Complete"
+
+    assert_not_nil BacklogItem.find_by_name_and_completed backlog_item.name, true
+  end
+
+  scenario "can reopen backlog item from the backlog that is completed" do
+    backlog_item = backlog_items :second_item
+    backlog_items(:first_item).delete
+
+    visit backlog_path
+    click_on "Reopen"
+
+    assert_not_nil BacklogItem.find_by_name_and_completed backlog_item.name, false
+  end
 end
