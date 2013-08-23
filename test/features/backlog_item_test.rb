073900b7f46a94424adcc7a0518ea0759e2c9b1a
Michael Avila <me@michaelavila.com>
2013-08-22 22:34:27 -0700

    Clean up backlog-item edit page

--- test/features/backlog_item_test.rb
+++ test/features/backlog_item_test.rb
@@ -13,7 +13,7 @@ feature "Backlog Item" do
     backlog_item = backlog_items :first_item
 
     visit backlog_item_path(backlog_item)
-    page.click_on "Delete"
+    page.find("#remove").click
 
     assert_nil BacklogItem.find_by_id backlog_item.id
   end
@@ -32,25 +32,27 @@ feature "Backlog Item" do
 
     visit backlog_item_path(backlog_item)
     fill_in "Name", :with => "Updated Backlog Item Name"
-    click_on "Update Backlog item"
+    click_on "Update"
 
     assert_not_nil BacklogItem.find_by_name("Updated Backlog Item Name")
   end
 
   scenario "can complete backlog item" do
     backlog_item = backlog_items :first_item
+    backlog_items(:second_item).delete
 
     visit backlog_item_path(backlog_item)
-    click_on "Complete"
+    page.find("#toggle").click
 
     assert_not_nil BacklogItem.find_by_name_and_completed backlog_item.name, true
   end
 
   scenario "can reopen backlog item that is completed" do
     backlog_item = backlog_items :second_item
+    backlog_items(:first_item).delete
 
     visit backlog_item_path(backlog_item)
-    click_on "Reopen"
+    page.find("#toggle").click
 
     assert_not_nil BacklogItem.find_by_name_and_completed backlog_item.name, false
   end
