4f8bf1014509d34c218e55f70433f77b933959a0
Brian Riley <brian@btriley.com>
2013-08-26 16:50:18 -0700

    Changed the tests to not use fixtures

--- spec/tasks/backlog_spec.rb
+++ spec/tasks/backlog_spec.rb
@@ -4,21 +4,11 @@ require 'rake'
 Backlog::Application.load_tasks
 
 describe "backlog:archive" do
-  fixtures :backlog_items
+  it "marks all complete items as archived" do
+    completed = mock_model BacklogItem, :completed => true
+    completed.should_receive(:update_attribute).with :archived, true
+    BacklogItem.should_receive(:where).with(:completed => true, :archived => false).and_return [completed]
 
-  before :each do
     Rake::Task['backlog:archive'].invoke
-  end
-
-  it "ignores non-completed items" do
-    backlog_item = backlog_items :first_item
-
-    assert_not_nil BacklogItem.find_by_id_and_archived(backlog_item.id, false)
-  end
-
-  it "sets archive to true on completed items" do
-    backlog_item = backlog_items :second_item
-
-    assert_not_nil BacklogItem.find_by_id_and_archived(backlog_item.id, true)
   end
 end
