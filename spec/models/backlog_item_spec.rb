057a541836395300c9bca1b649eee8ffa2f2ea1a
Michael Avila <me@michaelavila.com>
2013-08-26 05:42:31 -0700

    Add rspec test for backlog item model

---
+++ spec/models/backlog_item_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe BacklogItem do
+  fixtures :backlog_items
+
+  describe "creation" do
+    it "requires a name" do
+      BacklogItem.new.should_not be_valid
+
+      BacklogItem.new(:name => "something").should be_valid
+    end
+  end
+
+  describe "ordering" do
+    it "order" do
+      @item1 = backlog_items :first_item
+      @item1.update_attribute :created_at, DateTime.now
+
+      @item2 = backlog_items :second_item
+      @item2.update_attribute :created_at, DateTime.yesterday
+
+      items = BacklogItem.all
+      items.count.should eq(2)
+      items.first.should eq(@item2)
+      items.last.should eq(@item1)
+    end
+  end
+end
