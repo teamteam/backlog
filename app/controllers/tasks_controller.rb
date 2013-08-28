4f8be4c287226815cbdaf329aebb61a7b67e50cc
Brian Riley <brian@btriley.com>
2013-08-28 16:36:34 -0700

    TasksController spec fixes
    I removed some duplication in the specs and, I think, fixed the
    arel_table error that popped up every now and then.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,16 @@
 class TasksController < ApplicationController
   def new
     item = BacklogItem.find params[:backlog_item_id]
-    @task = item.tasks.build
+    @task = Task.new :backlog_item_id => item.id
   end
 
   def create
     item = BacklogItem.find params[:backlog_item_id]
     @task = Task.new :name => params[:task][:name], :backlog_item_id => item.id
-    @task.save
-    redirect_to backlog_item_path(item)
+    if @task.save
+      redirect_to backlog_item_path(item)
+    else
+      render :action => :new
+    end
   end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,48 +1,55 @@
 require 'spec_helper'
 
 describe TasksController do
+  let(:task) { mock_model(Task).as_null_object }
+  let(:item) { BacklogItem.create :name => "test backlog item name" }
+
+  before :each do
+    Task.stub(:new).and_return task
+  end
+
   describe "#new" do
-    before :each do
-      @item = BacklogItem.create :name => "test backlog item name"
-    end
-
     it "assigns a new task" do
-      get :new, :backlog_item_id => @item.id
-
-      expect(assigns :task).to be_a Task
-      expect(assigns :task).to be_new_record
+      get :new, :backlog_item_id => item.id
+      expect(assigns :task).to eq task
     end
 
     it "assigns new task to a backlog item" do
-      get :new, :backlog_item_id => @item.id
-
-      expect(assigns(:task).backlog_item).to eq(@item)
+      Task.should_receive(:new).with(:backlog_item_id => item.id)
+      get :new, :backlog_item_id => item.id
     end
   end
 
   describe "#create" do
-    before :each do
-      @item = BacklogItem.create :name => "test backlog item name"
-    end
-
     it "creates a new task" do
-      task = mock_model(Task).as_null_object
-      Task.should_receive(:new).with(:name => "My new task", :backlog_item_id => @item.id).and_return task
-      post :create, :backlog_item_id => @item.id, :task => { :name => "My new task" }
-    end
-
-    it "redirects to backlog item when creation is successful" do
-      post :create, :backlog_item_id => @item.id, :task => { :name => "My new task" }
-      expect(response).to redirect_to(@item)
+      Task.should_receive(:new).with(:name => "My new task", :backlog_item_id => item.id).and_return task
+      post :create, :backlog_item_id => item.id, :task => { :name => "My new task" }
     end
 
     it "saves the task" do
-      task = mock_model Task
-      Task.stub(:new).and_return(task)
       task.should_receive(:save)
-      post :create, :backlog_item_id => @item.id, :task => { :name => "My new task" }
+      post :create, :backlog_item_id => item.id, :task => { :name => "My new task" }
     end
 
-    it "renders the new template when creation fails"
+    context "when the task successfully saves" do
+      it "redirects to backlog item" do
+        post :create, :backlog_item_id => item.id, :task => { :name => "My new task" }
+        expect(response).to redirect_to(item)
+      end
+    end
+
+    context "when the task fails to save" do
+      it "assigns @task" do
+        task.stub(:save).and_return false
+        post :create, :backlog_item_id => item.id, :task => { :name => "My new task" }
+        expect(assigns :task).to eq(task)
+      end
+
+      it "renders the new template" do
+        task.stub(:save).and_return false
+        post :create, :backlog_item_id => item.id, :task => { :name => "My new task" }
+        expect(response).to render_template(:new)
+      end
+    end
   end
 end
