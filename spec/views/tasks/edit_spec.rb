d86dac6d281e3d61e9cf438f7ded8458ded378e9
Michael Avila <me@michaelavila.com>
2013-08-30 16:48:45 -0700

    Allow user to cancel when editing task name

--- spec/views/tasks/edit_spec.rb
+++ spec/views/tasks/edit_spec.rb
@@ -11,5 +11,6 @@ describe "tasks/edit" do
     expect(rendered).to have_selector "form[action='#{update_task_path(task.backlog_item, task)}']"
     expect(rendered).to have_field "Name"
     expect(rendered).to have_button "Update"
+    expect(rendered).to have_link("Cancel", href: backlog_item_path(task.backlog_item))
   end
 end
