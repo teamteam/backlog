ccbc95768b0ae3276d6d1010ea757c9facd4e1f2
Brian Riley <brian@btriley.com>
2013-09-02 21:39:03 -0700

    Move edit view to show

--- spec/views/tasks/show_spec.rb
+++ spec/views/tasks/show_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "tasks/edit" do
+describe "tasks/show" do
   it "has an edit task form" do
     task = mock_model Task
     task.stub(:backlog_item).and_return mock_model BacklogItem
@@ -8,7 +8,7 @@ describe "tasks/edit" do
 
     render
 
-    expect(rendered).to have_selector "form[action='#{update_task_path(task.backlog_item, task)}']"
+    expect(rendered).to have_selector "form[action='#{task_path(task.backlog_item, task)}']"
     expect(rendered).to have_field "Name"
     expect(rendered).to have_button "Update"
     expect(rendered).to have_link("Cancel", href: backlog_item_path(task.backlog_item))
